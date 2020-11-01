62f931f241b860df35218481a929197a71b90cea
21varun12 <56818932+21varun12@users.noreply.github.com>
2020-11-01 22:10:32 +0800

    Helps find color sensor calibration thresholds
    Need to add code to change RGB light color. Also need to find out if response time matters here. Testing code - might still have bugs!

---
+++ color_sensor_calibration.ino
@@ -0,0 +1,93 @@
+#include <MeMCore.h>
+
+// number of samples for color calibration
+#define NO_SAMPLES 100
+
+// response time of light sensor in ms (need to check this value using response_time.ino)
+#define RESPONSE_TIME 10
+
+// initialise peripheral objects
+MeLightSensor light_sensor(PORT_6);
+
+/* COLOR CODE FOR REFERENCE DURING CALIBRATION
+ * RED -> return 0
+   GREEN -> return 1
+   YELLOW -> return 2
+   PURPLE -> return 3
+   BLUE -> return 4 
+   WHITE -> return 5
+   BLACK -> return 6
+   OTHERS -> return -1 */
+
+void setup() {
+  // initialize serial communication at 9600 bps
+  Serial.begin(9600);
+
+  // read the light sensor values
+  int colorArray[7][NO_SAMPLES][3];
+
+  // iterate through the different colors
+  for (int i = 0; i < 7; i++) {
+    // print color papers status
+    Serial.print("Starting color ");
+    Serial.println(i);
+
+    // iterate through the different color channels
+    for (int j = 0; j < 3; j++){
+      // NEED TO ADD CODE TO SET R, G OR B LED LIGHT
+      //
+      //
+
+      // print color channel being used
+      Serial.print("Using color channel ");
+      Serial.print(j);
+      
+      // iterate through the different samples
+      for (int k = 0; k < NO_SAMPLES; k++){
+        colorArray[i][j][k] = get_sample();   // get average of 5 sensor readings
+
+        // print progress
+        if (j%10 == 0){
+          Serial.print("#");
+        }
+      }
+    }
+
+    Serial.println("Insert next color paper for calibration");
+    delay(7000);  // wait 7s before taking next measurements to give enough time for color paper change
+  }
+  Serial.println("Completed all");
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  // prints results in the following format <r0> <g0> <b0> <r1> ... <r6> <g6> <b6> 
+  // in comma seperated values form
+  for (int i = 0; i < NO_SAMPLES; i++){
+    for (int j = 0; j < 7; j++){
+      for (int k = 0; k < 3; k++){
+        Serial.print(colorArray[j][i][k]);
+        Serial.print(",");
+      }
+    }
+    // print the next set of values in another row
+    Serial.print();
+  }
+}
+
+void loop() {
+  // nothing here. everything done only once
+  break;
+}
+
+int get_sample(){
+  // takes 5 samples and returns the average
+  int no_samples = 5;
+  int sum = 0;
+
+  for (int i = 0; i < no_samples; i++){
+    delay(RESPONSE_TIME); // NEED TO CHECK IF THIS IS REALLY REQUIRED
+    sum += light_sensor.read();
+  }
+  return (sum / no_samples);
+}
