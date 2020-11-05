b166c9108e38bc5d24b7c11fafc513de732925ad
21varun12 <56818932+21varun12@users.noreply.github.com>
2020-11-05 10:44:44 +0800

    It finally works

--- color_sensor.ino
+++ color_sensor.ino
@@ -3,58 +3,18 @@
 // number of color sample measurements to take in each channel
 #define NO_SAMPLES 5
 
+#define BUTTON A7
+
 // response time for taking measurement in ms (NEED TO CHECK IF REQUIRED)
-#define RESPONSE_TIME 10
+#define RESPONSE_TIME 1000
 
 // set up threshold values the colors
-#define R_RED_UPPER 0
-#define R_RED_LOWER 0
-#define G_RED_UPPER 0
-#define G_RED_LOWER 0
-#define B_RED_UPPER 0
-#define B_RED_LOWER 0
+#define R_YELLOW_LOWER 75
+#define R_RED_LOWER 55
+#define B_BLUE_LOWER 65
+#define R_PURPLE_LOWER 35
+#define R_BLACK_UPPER 10
 
-#define R_GREEN_UPPER 0
-#define R_GREEN_LOWER 0
-#define G_GREEN_UPPER 0
-#define G_GREEN_LOWER 0
-#define B_GREEN_UPPER 0
-#define B_GREEN_LOWER 0
-
-#define R_YELLOW_UPPER 0
-#define R_YELLOW_LOWER 0
-#define G_YELLOW_UPPER 0
-#define G_YELLOW_LOWER 0
-#define B_YELLOW_UPPER 0
-#define B_YELLOW_LOWER 0
-
-#define R_PURPLE_UPPER 0
-#define R_PURPLE_LOWER 0
-#define G_PURPLE_UPPER 0
-#define G_PURPLE_LOWER 0
-#define B_PURPLE_UPPER 0
-#define B_PURPLE_LOWER 0
-
-#define R_BLUE_UPPER 0
-#define R_BLUE_LOWER 0
-#define G_BLUE_UPPER 0
-#define G_BLUE_LOWER 0
-#define B_BLUE_UPPER 0
-#define B_BLUE_LOWER 0
-
-#define R_WHITE_UPPER 0
-#define R_WHITE_LOWER 0
-#define G_WHITE_UPPER 0
-#define G_WHITE_LOWER 0
-#define B_WHITE_UPPER 0
-#define B_WHITE_LOWER 0
-
-#define R_BLACK_UPPER 0
-#define R_BLACK_LOWER 0
-#define G_BLACK_UPPER 0
-#define G_BLACK_LOWER 0
-#define B_BLACK_UPPER 0
-#define B_BLACK_LOWER 0
 
 // macros to control rgb
 #define RED_LED 255,000,000
@@ -82,14 +42,45 @@ MeRGBLed led(PORT_7);  // the internal connection is handled by port 7, with def
    BLACK -> return 6
    OTHERS -> return -1 */
 
+// colors for calibration
+Color black;
+Color white;
+
 void setup() {
   // initialize serial communication at 9600 bps
   Serial.begin(9600);
+
+  // perform color calibration first
+  Serial.println("Place white sample for calibration...");
+  delay(5000);
+  white = get_colors();
+
+  Serial.println("Place black sample for calibration...");
+  delay(5000);
+  black = get_colors();
+  Serial.println("Finished calibration");
+
+  // initialize start button
+  // press then release button to begin the run
+  pinMode(BUTTON, INPUT);
+
+  while (analogRead(BUTTON) > 500) {}
+  while (analogRead(BUTTON) < 500) {}
+  Serial.println("Waiting for button press");
 }
 
 void loop() {
+  delay(5000);
   // take color measurement
-  
+  Color new_color = get_colors();
+
+  // do color balancing
+  new_color.red = map(new_color.red, black.red, white.red, 0, 100);
+  new_color.green = map(new_color.green, black.green, white.green, 0, 100);
+  new_color.blue = map(new_color.blue, black.blue, white.blue, 0, 100);
+
+  // print out color detected
+  Serial.println(get_color_code(new_color));
 }
 
 int get_color_code(Color color) {
@@ -98,45 +89,41 @@ int get_color_code(Color color) {
   int red = color.red;
   int green = color.green;
   int blue = color.blue;
+  Serial.print(red);
+  Serial.print("\t");
+  Serial.print(green);
+  Serial.print("\t");
+  Serial.println(blue);
 
-  if (red < R_RED_UPPER && red > R_RED_LOWER && \
-      green < G_RED_UPPER && green > G_RED_LOWER  && \
-      blue < B_RED_UPPER && blue > B_RED_LOWER){
-    // RED
-    return 0;
-  } else if (red < R_GREEN_UPPER && red > R_GREEN_LOWER && \
-             green < G_GREEN_UPPER && green > G_GREEN_LOWER && \
-             blue < B_GREEN_UPPER && blue > B_GREEN_LOWER){
-    // GREEN
-    return 1;
-  } else if (red < R_YELLOW_UPPER && red > R_YELLOW_LOWER && \
-             green < G_YELLOW_UPPER && green > G_YELLOW_LOWER && \
-             blue < B_YELLOW_UPPER && blue > B_YELLOW_LOWER){
-    // YELLOW
+  if (red > R_YELLOW_LOWER)
+  {
+    // yellow
+    Serial.println("yellow");
     return 2;
-  } else if (red < R_PURPLE_UPPER && red > R_PURPLE_LOWER && \
-             green < G_PURPLE_UPPER && green > G_PURPLE_LOWER && \
-             blue < B_PURPLE_UPPER && blue > B_PURPLE_LOWER){
-    // PURPLE
-    return 3;
-  } else if (red < R_BLUE_UPPER && red > R_BLUE_LOWER && \
-             green < G_BLUE_UPPER && green > G_BLUE_LOWER && \
-             blue < B_BLUE_UPPER && blue > B_BLUE_LOWER){
-    // BLUE
+  } else if (red > R_RED_LOWER)
+  {
+    // red
+    Serial.println("red");
+    return 0;
+  } else if (blue > B_BLUE_LOWER)
+  {
+    // blue
+    Serial.println("blue");
     return 4;
-  } else if (red < R_WHITE_UPPER && red > R_WHITE_LOWER && \
-             green < G_WHITE_UPPER && green > G_WHITE_LOWER && \
-             blue < B_WHITE_UPPER && blue > B_WHITE_LOWER){
-    // WHITE
-    return 5;
-  } else if (red < R_BLACK_UPPER && red > R_BLACK_LOWER && \
-             green < G_BLACK_UPPER && green > G_BLACK_LOWER && \
-             blue < B_BLACK_UPPER && blue > B_BLACK_LOWER){
-    // BLACK
+  } else if (red > R_PURPLE_LOWER)
+  {
+    // purple
+    Serial.println("purple");
+    return 3;
+  } else if (red < R_BLACK_UPPER)
+  {
+    // black
+    Serial.println("black");
     return 6;
   } else {
-    // UNKNOWN COLOR: FAILED TO DETECT ANY OF THE ABOVE COLORS
-    return -1;
+    // green
+    Serial.println("green");
+    return 1;
   }
 }
 
@@ -148,7 +135,7 @@ Color get_colors() {
   led.setColor(RED_LED);
   led.show();
   color.red = get_single_color();
-
+ 
   // set RGB LED to GREEN
   led.setColor(GREEN_LED);
   led.show();
